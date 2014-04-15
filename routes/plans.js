f2e647de183b301fe9c6c627ee58f6ac00da8d72
muminoff <smuminov@gmail.com>
2014-04-15 17:07:40 +0900

    New version

--- routes/plans.js
+++ routes/plans.js
@@ -1,9 +1,9 @@
 var conn = require('../config')
-  , availablePlans = 'SELECT name, rate FROM available_plans';
+  , availablePlans = 'SELECT * FROM available_plans';
 
 exports.list = function(req, res){
   var connection = conn.index;
-  connection.query(available_plans, function(err, rows){
+  connection.query(availablePlans, function(err, rows){
     if(err) throw err;
     res.header('Access-Control-Allow-Origin', '*');
     res.json({available_plans: rows});
